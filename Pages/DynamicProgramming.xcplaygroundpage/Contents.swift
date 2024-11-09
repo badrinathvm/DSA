687d911f86ab75cce6c6a3ff7326faa91f619278
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2024-11-09 15:42:34 -0800

    Word Break problem

--- Pages/DynamicProgramming.xcplaygroundpage/Contents.swift
+++ Pages/DynamicProgramming.xcplaygroundpage/Contents.swift
@@ -7,3 +7,10 @@ let ballJumpingWays = BallJumpingStairs()
 let n = 5
 let ways = ballJumpingWays.ballJumpingStairs(n: n)
 print("Number of ways to reach step \(n):", ways)
+
+print("\n--- Word Breask Problem ---")
+let wordBreakProblem = WordBreak()
+let input = "leetcode"
+let wordDict: [String] = ["leet", "code"]
+let result = wordBreakProblem.wordBreak(s: input, wordDict: wordDict)
+print("Word Break:", result)
---
+++ Pages/DynamicProgramming.xcplaygroundpage/Sources/WordBreak.swift
@@ -0,0 +1,43 @@
+//
+//  WordBreak.swift
+//  
+//
+//  Created by Rani Badri on 11/9/24.
+//
+
+public class WordBreak {
+    public init() {}
+    
+    public func wordBreak(s: String, wordDict: [String]) -> Bool {
+        let wordSet = Set(wordDict)
+        
+        var dp = Array(repeating: false, count: s.count + 1)
+        dp[0] = true
+        
+        for i in 1...s.count {
+            for j in 0..<i {
+                // fetch start and end Index
+                let startIndex = s.index(s.startIndex, offsetBy: j)
+                let endIndex = s.index(s.startIndex, offsetBy: i)
+                // get teh substring
+                let subString = String(s[startIndex..<endIndex])
+                
+                // check if the subs tring exists in the wordDict Set
+                if dp[j] && wordSet.contains(subString) {
+                    dp[i] = true
+                    break
+                }
+            }
+        }
+        
+        return dp[s.count]
+    }
+}
+
+/*
+ Input: s = "leetcode", wordDict = ["leet", "code"]
+ 
+ Output: true
+
+ */
+
